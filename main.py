2bbc4199bd9152b80e86fbd1cf4ab6e5c66c44a4
Sachin Bulchandani <3011sachin@gmail.com>
2023-01-13 12:00:22 -0500

    corrected the nomenclature and updated to suppoert grid search

--- main.py
+++ main.py
@@ -385,7 +385,7 @@ if __name__ == "__main__":
             # "sgd_minibatch_size": 512,
             # "num_sgd_iter": 20,
             # "vf_loss_coeff": 0.0005,
-            # "vf_loss_coeff": tune.grid_search([0.001, 0.0005, 0.0009]),
+            "vf_loss_coeff": tune.grid_search([0.001, 0.0005, 0.0009]),
             # "vf_clip_param": 10,
             "lr": tune.grid_search([0.001, 0.0001])
             # "lr": 0.0001,
