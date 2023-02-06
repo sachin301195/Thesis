7fee71d23f90cccd149ffd0986cff57a02a64602
Sachin Bulchandani <3011sachin@gmail.com>
2023-02-06 17:32:28 -0500

    env updated dqN

--- main.py
+++ main.py
@@ -410,7 +410,7 @@ if __name__ == "__main__":
             #                                    tune.uniform(0.0001, 0.001), tune.uniform(0.0001, 0.001),
             #                                    tune.uniform(0.0001, 0.001)]),
             # "num_envs_per_worker": 4,
-            "horizon": 36,
+            # "horizon": 36,
             # "timesteps_per_batch": 2048,
         }
     else:
